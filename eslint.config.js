7d96f1f13209df3fb39dd218bfd2eb797031df46
Le Vivilet <levivlet@pm.me>
2025-02-08 10:53:15 +0100

    lint

--- eslint.config.js
+++ eslint.config.js
@@ -4,16 +4,6 @@ export default [
   ...config.default,
   {
     files: ['**/*.ts'],
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/ban-ts-comment': 'off',
-      '@typescript-eslint/no-unsafe-member-access': 'off',
-      '@typescript-eslint/no-unsafe-assignment': 'off',
-      '@typescript-eslint/no-unsafe-call': 'off',
-      '@typescript-eslint/no-unsafe-return': 'off',
-      '@typescript-eslint/no-unsafe-argument': 'off',
-      '@typescript-eslint/restrict-template-expressions': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
-    },
+    rules: {},
   },
 ]
