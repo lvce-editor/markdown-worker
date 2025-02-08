722caac06cd64af57f56c2d0308ee2d81c6bfe77
Le Vivilet <levivlet@pm.me>
2025-02-08 14:18:31 +0100

    feature: rename functions

--- packages/markdown-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/markdown-worker/src/parts/CommandMap/CommandMap.ts
@@ -3,7 +3,11 @@ import * as RenderMarkdown from '../RenderMarkdown/RenderMarkdown.ts'
 import * as Terminate from '../Terminate/Terminate.ts'
 
 export const commandMap = {
-  'Markdown.getMarkDownVirtualDom': GetMarkdownVirtualDom.getMarkdownVirtualDom,
-  'Markdown.renderMarkdown': RenderMarkdown.renderMarkdown,
+  'Markdown.getVirtualDom': GetMarkdownVirtualDom.getMarkdownVirtualDom,
+  'Markdown.render': RenderMarkdown.renderMarkdown,
   'Markdown.terminate': Terminate.terminate,
+
+  // deprecated
+  'Markdown.renderMarkdown': RenderMarkdown.renderMarkdown,
+  'Markdown.getMarkDownVirtualDom': GetMarkdownVirtualDom.getMarkdownVirtualDom,
 }
