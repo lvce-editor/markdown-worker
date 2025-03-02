ec738df2b65cb68825c5ef50ca543bd6e71fdf55
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-02 18:27:36 +0100

    feature: add markdown test (#10)

---
+++ packages/markdown-worker/test/RenderMarkdown.test.ts
@@ -0,0 +1,30 @@
+import { expect, test } from '@jest/globals'
+import * as RenderMarkdown from '../src/parts/RenderMarkdown/RenderMarkdown.ts'
+
+test('empty string', async () => {
+  expect(await RenderMarkdown.renderMarkdown('')).toBe('')
+})
+
+test('plain text', async () => {
+  expect(await RenderMarkdown.renderMarkdown('hello')).toBe('<p>hello</p>\n')
+})
+
+test('heading', async () => {
+  expect(await RenderMarkdown.renderMarkdown('# hello')).toBe('<h1>hello</h1>\n')
+})
+
+test('link', async () => {
+  expect(await RenderMarkdown.renderMarkdown('[test](https://example.com)')).toBe('<p><a href="https://example.com">test</a></p>\n')
+})
+
+test('code block', async () => {
+  expect(await RenderMarkdown.renderMarkdown('```\ncode\n```')).toBe('<pre><code>code\n</code></pre>\n')
+})
+
+test('list', async () => {
+  expect(await RenderMarkdown.renderMarkdown('- item 1\n- item 2')).toBe('<ul>\n<li>item 1</li>\n<li>item 2</li>\n</ul>\n')
+})
+
+test('blockquote', async () => {
+  expect(await RenderMarkdown.renderMarkdown('> quote')).toBe('<blockquote>\n<p>quote</p>\n</blockquote>\n')
+})
