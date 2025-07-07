732aa4aa5adaf335232a353bc016438824b590a0
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-07 15:55:46 +0200

    feature: add handleMessagePort function (#58)
    * feature: add handleMessagePort function
    
    * coverage

--- packages/markdown-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/markdown-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,9 +1,11 @@
 import { terminate } from '@lvce-editor/viewlet-registry'
 import * as GetMarkdownVirtualDom from '../GetMarkdownVirtualDom/GetMarkdownVirtualDom.ts'
+import * as HandleMessagePort from '../HandleMessagePort/HandleMessagePort.ts'
 import * as RenderMarkdown from '../RenderMarkdown/RenderMarkdown.ts'
 
 export const commandMap = {
   'Markdown.getVirtualDom': GetMarkdownVirtualDom.getMarkdownVirtualDom,
+  'Markdown.handleMessagePort': HandleMessagePort.handleMessagePort,
   'Markdown.render': RenderMarkdown.renderMarkdown,
   'Markdown.terminate': terminate,
 
