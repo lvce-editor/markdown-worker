00f3fe87f83b1308a1a9a28d9ea5feee07c28c39
Le Vivilet <levivilet@pm.me>
2026-01-13 21:03:00 +0100

    move

--- packages/markdown-worker/src/parts/GetMarkdownVirtualDom/GetMarkdownVirtualDom.ts
+++ packages/markdown-worker/src/parts/GetMarkdownVirtualDom/GetMarkdownVirtualDom.ts
@@ -1,15 +1,12 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import type { MarkdownVirtualDomOptions } from '../MarkdownVirtualDomOptions/MarkdownVirtualDomOptions.ts'
 import * as AllowedMarkdownAttributes from '../AllowedMarkdownAttributes/AllowedMarkdownAttributes.ts'
 import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
 import * as Assert from '../Assert/Assert.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as GetVirtualDomChildCount from '../GetVirtualDomChildCount/GetVirtualDomChildCount.ts'
 import * as ParseHtml from '../ParseHtml/ParseHtml.ts'
-
-interface MarkdownVirtualDomOptions {
-  readonly onContextMenu?: string | number
-}
 
 export const getMarkdownVirtualDom = (html: string, { onContextMenu = undefined }: MarkdownVirtualDomOptions = {}): readonly VirtualDomNode[] => {
   Assert.string(html)
---
+++ packages/markdown-worker/src/parts/MarkdownVirtualDomOptions/MarkdownVirtualDomOptions.ts
@@ -0,0 +1,3 @@
+export interface MarkdownVirtualDomOptions {
+  readonly onContextMenu?: string | number
+}
