bd5c9be6ed513e5df5a31cbef2c93700c06ee1a0
Le Vivilet <levivilet@pm.me>
2026-07-13 08:22:29 +0000

    fix: update lint rules and workflows

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,14 @@
 import * as config from '@lvce-editor/eslint-config'
 import * as actions from '@lvce-editor/eslint-plugin-github-actions'
 
-export default [...config.default, ...actions.default]
+export default [
+  ...config.default,
+  ...actions.default,
+  {
+    rules: {
+      'sonarjs/assertions-in-tests': 'off',
+      'unicorn/no-break-in-nested-loop': 'off',
+      'unicorn/no-global-object-property-assignment': 'off',
+    },
+  },
+]
