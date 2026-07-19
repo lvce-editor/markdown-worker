a0a74e66647cdbfee497a50e16d7cf27a6d6d674
Le Vivilet <levivilet@pm.me>
2026-07-19 14:33:31 +0000

    feature: enable recommended virtual dom lint rules

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,21 @@ import * as actions from '@lvce-editor/eslint-plugin-github-actions'
 
 export default [
   ...config.default,
+  ...config.recommendedVirtualDom,
   ...actions.default,
   {
     rules: {
+      'jest/expect-expect': 'off',
+      'jest/no-disabled-tests': 'off',
       'sonarjs/assertions-in-tests': 'off',
       'unicorn/no-break-in-nested-loop': 'off',
       'unicorn/no-global-object-property-assignment': 'off',
+    },
+  },
+  {
+    files: ['packages/markdown-worker/test/**/*.ts'],
+    rules: {
+      'virtual-dom/prefer-constants': 'off',
     },
   },
 ]
