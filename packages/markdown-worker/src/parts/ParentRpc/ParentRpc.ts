c787374e5309e37f5afd88715a472c190452a5b8
Le Vivilet <levivilet@pm.me>
2026-08-01 16:03:10 -0700

    feature: update eslint dependencies (#135)
    * feature: update eslint dependencies
    
    * fix: allow memory measurement variance

--- packages/markdown-worker/src/parts/ParentRpc/ParentRpc.ts
+++ packages/markdown-worker/src/parts/ParentRpc/ParentRpc.ts
@@ -1,3 +1,3 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
-export const { set } = RendererWorker
+export const set = (...args: Readonly<Parameters<typeof RendererWorker.set>>): ReturnType<typeof RendererWorker.set> => RendererWorker.set(...args)
